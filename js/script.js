cf160afb25660fafb3aa27d579047482be87d9af
sumitrishi01 <sumitrishi01@gmail.com>
2023-06-14 21:51:47 +0530

    javascript

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+//1. function defination is a one time process
+function greetMyStudents(name){ //I will receive the formal argument
+    console.log("Hello Goodmorning "+name); 
+}
+
+
+//2. Invoke/Call   Invoking/Calling
+// Function calling is a many time proces
+greetMyStudents("Sumit");  //we can pass the actual argument
+greetMyStudents('Rishi');
+greetMyStudents(`Sahil`);
---
+++ js/script1.js
@@ -0,0 +1,11 @@
+
+//1. function defination area
+function addTwoNumber(n1,n2){
+    console.log(n1+n2)
+}
+
+//2. invoke/call
+addTwoNumber(10,53); //parameter/argument
+addTwoNumber(20,40); //parameter/argument 
+addTwoNumber(40,60); //parameter/argument 
+addTwoNumber(80,70); //parameter/argument 
